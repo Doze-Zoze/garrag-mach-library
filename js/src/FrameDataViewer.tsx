f5a88672abffbd254064dd4b82756cebb044d9b7
Joshua Saunders <jk.saunders@hotmail.com>
2021-09-29 18:30:32 -0600

    Correct height/width when patching

--- js/src/FrameDataViewer.tsx
+++ js/src/FrameDataViewer.tsx
@@ -27,13 +27,15 @@ const loadGIF = (url): Promise<ParsedFrame[]> =>
 
 const FrameDataViewer: FC<FrameDataViewerProps> = ({ id }) => {
   const [frames, setFrames] = useState<ParsedFrame[] | null>(null);
-  const [frameCount, setFrameCount] = useState(0);
+  const [frameIndex, setFrameIndex] = useState(0);
   const [playing, setIsPlaying] = useState(false);
 
   const tempCanvasId = `frame-data-viewer-temp-canvas-${id}`;
   const canvasId = `frame-data-viewer-canvas-${id}`;
-  const tempCanvasRef = useRef<HTMLCanvasElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const tempCanvasRef = useRef<HTMLCanvasElement | null>(null);
+  const fullGifCanvasRef = useRef<HTMLCanvasElement | null>(null);
+  const frameImageDataRef = useRef<ImageData | null>(null);
 
   useEffect(() => {
     (async (): Promise<void> => {
@@ -45,26 +47,107 @@ const FrameDataViewer: FC<FrameDataViewerProps> = ({ id }) => {
   }, []);
 
   useEffect(() => {
+    // var c === const canvas
+    // var ctx === canvasContext
+    // var tempCanvas === tempCanvasRef.current
+    // var gifCanvas === fullGifCanvasRef.current
+    // var frameImageData === frameImageDataRef.current
+
+    if (tempCanvasRef.current == null) {
+      tempCanvasRef.current = document.createElement('canvas');
+    }
+    if (fullGifCanvasRef.current == null) {
+      fullGifCanvasRef.current = document.createElement('canvas');
+    }
+
     const canvas = canvasRef.current;
     const tempCanvas = tempCanvasRef.current;
-    if (frames != null && canvas != null && tempCanvas != null) {
-      const canvasContext = canvas.getContext('2d');
-      const tempContext = canvas.getContext('2d');
+    const fullGifCanvas = fullGifCanvasRef.current;
 
-      if (canvasContext && tempContext) {
-        const frame = frames[frameCount];
-        const imageData = canvasContext.createImageData(
-          frame.dims.width,
-          frame.dims.height
-        );
-        canvas.height = frame.dims.height;
-        canvas.width = frame.dims.width;
-        imageData.data.set(frame.patch);
-
-        canvasContext.putImageData(imageData, 0, 0);
-      }
+    if (
+      frames == null ||
+      canvas == null ||
+      tempCanvas == null ||
+      fullGifCanvas == null
+    ) {
+      return;
     }
-  }, [frames == null, frameCount]);
+
+    const canvasCtx = canvas.getContext('2d');
+    const tempCanvasCtx = tempCanvas.getContext('2d');
+    const fullGifCanvasCtx = fullGifCanvas.getContext('2d');
+
+    if (
+      canvasCtx == null ||
+      tempCanvasCtx == null ||
+      fullGifCanvasCtx == null
+    ) {
+      return;
+    }
+
+    canvas.width = frames[0].dims.width;
+    canvas.height = frames[0].dims.height;
+    fullGifCanvas.width = canvas.width;
+    fullGifCanvas.height = canvas.height;
+
+    const frame = frames[frameIndex];
+    if (frame.disposalType === 2) {
+      fullGifCanvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    const dims = frame.dims;
+
+    if (
+      !frameImageDataRef.current ||
+      dims.width != frameImageDataRef.current.width ||
+      dims.height != frameImageDataRef.current.height
+    ) {
+      tempCanvas.width = dims.width;
+      tempCanvas.height = dims.height;
+      frameImageDataRef.current = tempCanvasCtx.createImageData(
+        dims.width,
+        dims.height
+      );
+    }
+
+    // set the patch data as an override
+    frameImageDataRef.current.data.set(frame.patch);
+
+    // draw the patch back over the canvas
+    tempCanvasCtx.putImageData(frameImageDataRef.current, 0, 0);
+    fullGifCanvasCtx.drawImage(tempCanvas, dims.left, dims.top);
+
+    const imageData = fullGifCanvasCtx.getImageData(
+      0,
+      0,
+      fullGifCanvas.width,
+      fullGifCanvas.height
+    );
+
+    canvasCtx.putImageData(imageData, 0, 0);
+    // canvasCtx.drawImage(
+    //   canvas,
+    //   0,
+    //   0,
+    //   canvas.width,
+    //   canvas.height,
+    //   0,
+    //   0,
+    //   pixelsX,
+    //   pixelsY
+    // );
+    // canvasCtx.drawImage(
+    //   canvas,
+    //   0,
+    //   0,
+    //   pixelsX,
+    //   pixelsY,
+    //   0,
+    //   0,
+    //   canvas.width,
+    //   canvas.height
+    // );
+  }, [frames == null, frameIndex]);
 
   if (!frames) {
     return <div>Loading!</div>;
@@ -73,11 +156,10 @@ const FrameDataViewer: FC<FrameDataViewerProps> = ({ id }) => {
   return (
     <div>
       <h1>{id}</h1>
-      <canvas id={tempCanvasId} ref={tempCanvasRef}></canvas>
       <canvas id={canvasId} ref={canvasRef}></canvas>
       <button
         onClick={(): void =>
-          setFrameCount((n) => (n < frames.length - 1 ? n + 1 : 0))
+          setFrameIndex((n) => (n < frames.length - 1 ? n + 1 : 0))
         }
       >
         Next frame
